d026fdd348bc677eb72cef0c617f4af647562632
aleksichen <aleksichen@gmail.com>
2024-09-11 16:13:42 +0800

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Plan from "@/pages/plan";
 import PlanDetail from "@/pages/plan-detail";
 import SignUp from "./pages/signup";
@@ -7,7 +7,7 @@ import SignIn from "./pages/signin";
 export const routes = [
   {
     path: "/",
-    element: <Plan />,
+    element: <Navigate to="/signin" replace />,  // 默认重定向到 /signin
   },
   {
     path: "/signin",
